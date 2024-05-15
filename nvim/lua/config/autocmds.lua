7cfbd1fdfb613373865568ab11ce05c58498ae85
cxzhou35 <zjajzcx3566@163.com>
2024-05-15 19:16:27 +0800

    [#neovim] fix terminal cursor bug

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -102,3 +102,9 @@ vim.api.nvim_create_autocmd("FileType", {
   end,
   group = _ft,
 })
+
+vim.api.nvim_create_autocmd({ "ExitPre" }, {
+  callback = function()
+    vim.opt.guicursor = "a:ver30-blinkon0"
+  end,
+})
---
+++ setup/sync.sh
@@ -0,0 +1,44 @@
+#! /bin/bash
+
+# get current direction
+curDir=$(pwd)
+
+# get shell direction
+workDir=$(
+	cd "$(dirname "$0")"
+	pwd
+)
+
+cd $workDir
+
+echo "Working on $workDir"
+
+# check if there is any changes
+if [ -z "$(git status --porcelain)" ]; then
+	echo "No changes to commit"
+	exit 0
+fi
+
+git add -A
+
+if [ $# -gt 0 ]; then
+	if [ $# -eq 2 ]; then
+		git commit -m "[#$1] $2"
+	else
+		git commit -m "[+] $@"
+	fi
+else
+	git commit -m "[Default] Sync Local Files"
+fi
+
+git push
+
+# check if fail
+if [ $? -ne 0 ]; then
+	echo "Sync Failed!"
+	exit 1
+else
+	echo "Sync Success!"
+fi
+
+cd $curDir

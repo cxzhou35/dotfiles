3abc42f2fa3a55d6b6910bab63dd4537ef3f192a
cxzhou35 <zjajzcx3566@163.com>
2024-05-16 23:31:17 +0800

    [#neovim] update autocmd for markdown

--- nvim/lua/config/autocmds.lua
+++ nvim/lua/config/autocmds.lua
@@ -58,11 +58,17 @@ vim.api.nvim_create_autocmd("FileType", {
 -- Wrap and check for spell in text filetypes
 vim.api.nvim_create_autocmd("FileType", {
   group = augroup("wrap_spell"),
-  pattern = { "tex" },
+  pattern = { "tex", "gitcommit", "markdown", "pandoc" },
   callback = function()
     vim.opt_local.wrap = true
-    vim.opt_local.spell = true
+    vim.opt_local.spell = false
   end,
+})
+
+vim.api.nvim_create_autocmd("filetype", {
+  -- group = augroup("wrap_spell"),
+  pattern = { "gitcommit", "markdown", "pandoc" },
+  command = "set nospell",
 })
 
 -- Auto sync plugins on save

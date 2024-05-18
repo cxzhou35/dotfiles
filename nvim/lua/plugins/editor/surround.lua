5e8d5d5caccfb8227251f03d56a19cf077aba1a5
cxzhou35 <zjajzcx3566@163.com>
2024-05-18 08:45:15 +0800

    [#neovim] update surround plugin config

--- nvim/lua/plugins/editor/surround.lua
+++ nvim/lua/plugins/editor/surround.lua
@@ -8,9 +8,19 @@ return {
         find = "gsf",
         find_left = "gsF",
         highlight = "gsh",
-        replace = "gsr",
+        replace = "gsc",
         update_n_lines = "gsn",
       },
+      silent = true,
     },
+  },
+  {
+    "kylechui/nvim-surround",
+    enabled = false,
+    version = "*", -- Use for stability; omit to use `main` branch for the latest features
+    event = "VeryLazy",
+    config = function()
+      require("nvim-surround").setup({})
+    end,
   },
 }

025c729d2b3fffda13bd8bca1f3484db53123ea7
cxzhou35 <zjajzcx3566@163.com>
2024-08-09 13:52:36 +0800

    [#yazi] update key bindings

--- yazi/init.lua
+++ yazi/init.lua
@@ -1,31 +1,25 @@
-function Manager:render(area)
-	local chunks = self:layout(area)
-
-	local bar = function(c, x, y)
-		x, y = math.max(0, x), math.max(0, y)
-		return ui.Bar(ui.Rect({ x = x, y = y, w = ya.clamp(0, area.w - x, 1), h = math.min(1, area.h) }), ui.Bar.TOP)
-			:symbol(c)
+-- Show symlink in status bar
+Status:children_add(function()
+	local h = cx.active.current.hovered
+	if h == nil or ya.target_family() ~= "unix" then
+		return ui.Line({})
 	end
 
-	return ya.flat({
-		-- Borders
-		ui.Border(area, ui.Border.ALL):type(ui.Border.ROUNDED),
-		ui.Bar(chunks[1], ui.Bar.RIGHT),
-		ui.Bar(chunks[3], ui.Bar.LEFT),
-
-		bar("┬", chunks[1].right - 1, chunks[1].y),
-		bar("┴", chunks[1].right - 1, chunks[1].bottom - 1),
-		bar("┬", chunks[2].right, chunks[2].y),
-		bar("┴", chunks[2].right, chunks[1].bottom - 1),
-
-		-- Parent
-		Parent:render(chunks[1]:padding(ui.Padding.xy(1))),
-		-- Current
-		Current:render(chunks[2]:padding(ui.Padding.y(1))),
-		-- Preview
-		Preview:render(chunks[3]:padding(ui.Padding.xy(1))),
+	return ui.Line({
+		ui.Span(ya.user_name(h.cha.uid) or tostring(h.cha.uid)):fg("magenta"),
+		ui.Span(":"),
+		ui.Span(ya.group_name(h.cha.gid) or tostring(h.cha.gid)):fg("magenta"),
+		ui.Span(" "),
 	})
-end
+end, 500, Status.RIGHT)
+
+-- Show user/group of files in status bar
+Header:children_add(function()
+	if ya.target_family() ~= "unix" then
+		return ui.Line({})
+	end
+	return ui.Span(ya.user_name() .. "@" .. ya.host_name() .. ":"):fg("blue")
+end, 500, Header.LEFT)
 
 require("starship"):setup()
 

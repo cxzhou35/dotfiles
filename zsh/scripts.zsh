8462ec77d0fcf7c7eb7ba65a72d49a64a176ca60
cxzhou35 <zjajzcx3566@163.com>
2024-09-03 11:39:02 +0800

    [#zsh] update yazi script alias

--- zsh/scripts.zsh
+++ zsh/scripts.zsh
@@ -56,7 +56,7 @@ function sc(){
   sesh connect "$(sesh list -i | gum filter --limit 1 --placeholder 'Pick a sesh' --prompt='⚡')"
 }
 
-function ye() {
+function ys() {
   local tmp="$(mktemp -t "yazi-cwd.XXXXXX")"
   yazi "$@" --cwd-file="$tmp"
   if cwd="$(cat -- "$tmp")" && [ -n "$cwd" ] && [ "$cwd" != "$PWD" ]; then

8ddd803bb379c736f131f6c59b7c95d62665c8b0
cxzhou35 <zjajzcx3566@163.com>
2024-12-04 11:56:29 +0800

    [#neovim] add cursor animate plugin

---
+++ nvim/lua/plugins/utils/cursor.lua
@@ -0,0 +1,11 @@
+return {
+  "sphamba/smear-cursor.nvim",
+  event = "VeryLazy",
+  opts = {
+    smear_between_buffers = true,
+    smear_between_neighbor_lines = false,
+    legacy_computing_symbols_support = false,
+    hide_target_hack = true,
+    cursor_color = "none",
+  },
+}

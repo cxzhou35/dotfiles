dbe2632745edd72b37749afb3df435d4766ddfe2
cxzhou35 <zjajzcx3566@163.com>
2024-12-27 20:53:52 +0800

    [#yazi] update yazi configs

--- yazi/init.lua
+++ yazi/init.lua
@@ -32,6 +32,29 @@ require("full-border"):setup({
 	type = ui.Border.ROUNDED,
 })
 
+require("git"):setup()
+
+require("mactag"):setup({
+	-- Keys used to add or remove tags
+	keys = {
+		r = "Red",
+		o = "Orange",
+		y = "Yellow",
+		g = "Green",
+		b = "Blue",
+		p = "Purple",
+	},
+	-- Colors used to display tags
+	colors = {
+		Red = "#ee7b70",
+		Orange = "#f5bd5c",
+		Yellow = "#fbe764",
+		Green = "#91fc87",
+		Blue = "#5fa3f8",
+		Purple = "#cb88f8",
+	},
+})
+
 require("zoxide"):setup({
 	update_db = true,
 })

c162197020daf19a0e9929c5b6599aaa906f1835
cxzhou35 <zjajzcx3566@163.com>
2025-04-30 16:19:22 +0800

    [#neovim] update copilot chat configs

--- nvim/lua/plugins/code/copilot.lua
+++ nvim/lua/plugins/code/copilot.lua
@@ -30,28 +30,81 @@ return {
   },
   {
     "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "canary",
-    cmd = "CopilotChat",
+    branch = "main",
+    event = "VeryLazy",
+    -- cmd = { "CopilotChat", "CopilotChatModels", "CopilotChatAgents" },
     keys = { { "<leader>cu", "<cmd>CopilotChatToggle<cr>", desc = "Toggle chat window" } },
     dependencies = {
       { "zbirenbaum/copilot.lua" },
       { "nvim-lua/plenary.nvim" },
     },
-    build = "make tiktoken", -- Only on MacOS or Linux
-    opts = {
-      config = function()
-        require("CopilotChat").setup({
-          debug = false,
-          show_folds = true,
-          highlight_selection = true,
-          mappings = {
-            complete = {
-              insert = "",
-            },
+    -- build = "make tiktoken", -- Only on MacOS or Linux
+    opts = function()
+      local user = vim.env.USER or "User"
+      user = user:sub(1, 1):upper() .. user:sub(2)
+      return {
+        -- model = "gpt-4o-2024-11-20",
+        -- model = "gpt-4-o-preview",
+        model = "claude-3.7-sonnet-thought",
+        agent = "copilot",
+        auto_insert_mode = false,
+        debug = false,
+        question_header = "  " .. user .. " ",
+        answer_header = "  Copilot ",
+        show_folds = true,
+        window = {
+          width = 0.4,
+        },
+        mappings = {
+          -- Use tab for completion
+          complete = {
+            detail = "Use @<Tab> or /<Tab> for options.",
+            insert = "<Tab>",
+          },
+          submit_prompt = {
+            normal = "<CR>",
+            insert = "<C-s>",
+          },
+          close = {
+            normal = "q",
+            insert = "<C-q>",
+          },
+        },
+      }
+    end,
+    config = function(_, opts)
+      local chat = require("CopilotChat")
+      chat.setup(opts)
+
+      local select = require("CopilotChat.select")
+
+      -- Inline chat with Copilot
+      vim.api.nvim_create_user_command("CopilotChatInline", function(args)
+        chat.ask(args.args, {
+          selection = select.visual,
+          window = {
+            layout = "float",
+            relative = "cursor",
+            width = 1,
+            height = 0.4,
+            row = 1,
           },
         })
-      end,
-    },
+      end, { nargs = "*", range = true })
+
+      vim.api.nvim_create_autocmd("BufEnter", {
+        pattern = "copilot-chat",
+        callback = function()
+          vim.opt_local.relativenumber = false
+          vim.opt_local.number = false
+        end,
+      })
+
+      -- Restore CopilotChatBuffer
+      vim.api.nvim_create_user_command("CopilotChatBuffer", function(args)
+        chat.ask(args.args, { selection = select.buffer })
+      end, { nargs = "*", range = true })
+    end,
   },
   -- lualine integrations
   {

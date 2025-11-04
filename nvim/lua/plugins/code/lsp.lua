aec70ddf7be8a6954d03e2c988aeaaba70285052
cxzhou35 <zjajzcx3566@163.com>
2025-11-04 21:23:14 +0800

    update ghostty configs and replace iterm2 with it

--- nvim/lua/plugins/code/lsp.lua
+++ nvim/lua/plugins/code/lsp.lua
@@ -1,12 +1,12 @@
 return {
   {
     "neovim/nvim-lspconfig",
-    init = function()
-      local keys = require("lazyvim.plugins.lsp.keymaps").get()
-      keys[#keys + 1] = { "K", false }
-      keys[#keys + 1] = { "gr", false }
-      keys[#keys + 1] = { "gy", false }
-    end,
+    -- init = function()
+    --   local keys = require("lazyvim.plugins.lsp.keymaps").get()
+    --   keys[#keys + 1] = { "K", false }
+    --   keys[#keys + 1] = { "gr", false }
+    --   keys[#keys + 1] = { "gy", false }
+    -- end,
     opts = {
       diagnostics = {
         underline = true,
